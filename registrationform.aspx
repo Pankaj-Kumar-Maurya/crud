8f2a57c4de90763c7cb86e031db7fbb35cc663f6
Pankaj Kumar Maurya <68053586+Pankaj-Kumar-Maurya@users.noreply.github.com>
2022-03-28 22:38:01 +0530

    Add files via upload

---
+++ registrationform.aspx
@@ -0,0 +1,116 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="registrationform.aspx.cs" Inherits="registration.registrationform" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                <td>Name :</td>
+                <td><asp:TextBox ID="txtname" runat="server"></asp:TextBox></td>
+                    </tr>
+                <tr>
+                <td>Blood Group</td>
+                <td><asp:RadioButtonList ID="rblbg" runat="server" RepeatColumns="4">
+                    <asp:ListItem Text="AB" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="A" Value="2"></asp:ListItem>
+                    <asp:ListItem Text="B" Value="3"></asp:ListItem>
+                    <asp:ListItem Text="O" Value="4"></asp:ListItem>
+
+                    </asp:RadioButtonList></td>
+                    </tr>
+                <tr>
+                <td>Qulification :</td>
+                <td><asp:DropDownList ID="ddlqli" runat="server">
+                    <%--<asp:ListItem Text="--Select" Value="0"></asp:ListItem>
+                     <asp:ListItem Text="BCA" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="MCA" Value="2"></asp:ListItem>
+                    <asp:ListItem Text="B.Tech" Value="3"></asp:ListItem>--%>
+                    
+                    </asp:DropDownList></td>
+                    </tr>
+                <tr>
+                <td>Country </td>
+                <td><asp:DropDownList ID="ddlcountry" runat="server" OnSelectedIndexChanged="ddlcountry_SelectedIndexChanged" AutoPostBack="true">
+                   <%--<asp:ListItem Text="--Select" Value="0"></asp:ListItem>
+                     <asp:ListItem Text="India" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="Japan" Value="2"></asp:ListItem>
+                    <asp:ListItem Text="Russia" Value="3"></asp:ListItem>--%>
+
+                    </asp:DropDownList></td>
+                    </tr>
+                <tr>
+                    <td>State </td>
+                    <td><asp:DropDownList ID="ddlstate" runat="server">
+
+                        </asp:DropDownList></td>
+                </tr>
+                <tr>
+                <td></td>
+                <td><asp:Button ID="btnsave" runat="server" Text="Save" OnClick="btnsave_Click" /></td>
+                    </tr>
+                <tr>
+                    <td></td>
+                    <td><asp:GridView ID="grid" runat="server" AutoGenerateColumns="false" OnRowCommand="grid_RowCommand">
+                        <Columns>
+                            <asp:TemplateField HeaderText="Form Id">
+                                <ItemTemplate>
+                                    <%#Eval("id") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                             <asp:TemplateField HeaderText="Form Name">
+                                <ItemTemplate>
+                                    <%#Eval("name") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                             <asp:TemplateField HeaderText="Blood Group">
+                                <ItemTemplate>
+                                    <%#Eval("bloodgroup").ToString()=="1"?"AB":Eval("bloodgroup").ToString()=="2"?"A":Eval("bloodgroup").ToString()=="3"?"B":"O" %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                             <asp:TemplateField HeaderText="Qulification">
+                                <ItemTemplate>
+                                    <%#Eval("qname") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                             <asp:TemplateField HeaderText="Country">
+                                <ItemTemplate>
+                                    <%#Eval("cname") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                            <asp:TemplateField HeaderText="State">
+                                <ItemTemplate>
+                                    <%#Eval("sname") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                             <asp:TemplateField >
+                                <ItemTemplate>
+                                    <asp:Button ID="btndelete" runat="server" Text="Delete" CommandName="A" CommandArgument='<%#Eval("id") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                            <asp:TemplateField >
+                                <ItemTemplate>
+                                    <asp:Button ID="btnedit" runat="server" Text="Edit" CommandName="B" CommandArgument='<%#Eval("id") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+
+                        </asp:GridView></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ registrationform.aspx.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace registration
+{
+    public partial class registrationform : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("data source=LAPTOP-M07BDFTM;initial catalog=registration ;integrated security=true");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                show();
+                showquli();
+                showcountry();
+                ddlstate.Enabled = false;
+                ddlstate.Items.Insert(0, new ListItem("--Select--", "0"));
+            }
+        }
+
+        public void clear()
+        {
+            txtname.Text = "";
+            rblbg.ClearSelection();
+            ddlqli.SelectedValue = "0";
+            ddlcountry.SelectedValue = "0";
+            ddlstate.SelectedValue = "0";
+        }
+        public void show()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from form join tblquli on qulification = qid join tblcountry on country = cid join tblstate on state=sid ", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            grid.DataSource = dt;
+            grid.DataBind();
+
+        }
+
+        public void showquli()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from tblquli", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            ddlqli.DataValueField = "qid";
+            ddlqli.DataTextField = "qname";
+            ddlqli.DataSource = dt;
+            ddlqli.DataBind();
+            ddlqli.Items.Insert(0, new ListItem("--Select--", "0"));
+
+        }
+
+        public void showcountry()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from tblcountry", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            ddlstate.Enabled = false;
+            ddlcountry.DataValueField = "cid";
+            ddlcountry.DataTextField = "cname";
+            ddlcountry.DataSource = dt;
+            ddlcountry.DataBind();
+            ddlcountry.Items.Insert(0, new ListItem("--Select--", "0"));
+
+        }
+
+        public void showstate()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from tblstate where cid='"+ddlcountry.SelectedValue+"'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            ddlstate.DataValueField = "sid";
+            ddlstate.DataTextField = "sname";
+            ddlstate.DataSource = dt;
+            ddlstate.DataBind();
+            ddlstate.Items.Insert(0, new ListItem("--Select--", "0"));
+
+        }
+
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (btnsave.Text == "Save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert form values('" + txtname.Text + "','" + rblbg.SelectedValue + "','" + ddlqli.SelectedValue + "','"+ddlcountry.SelectedValue+"','"+ddlstate.SelectedValue+"')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                show();
+            }
+            else if(btnsave.Text=="Update")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update form set name='"+txtname.Text+"', bloodgroup='"+rblbg.SelectedValue+ "', qulification='"+ddlqli.SelectedValue+"', country='"+ddlcountry.SelectedValue+"',state='"+ddlstate.SelectedValue+"' where id='"+ViewState["kk"]+"'  ", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                show();
+                btnsave.Text = "Save";
+            }
+            clear();
+        }
+
+        protected void grid_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "A")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete form where id='" + e.CommandArgument + "'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                show();
+            }
+            else if(e.CommandName=="B")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from form where id='"+e.CommandArgument+"'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                con.Close();
+                txtname.Text = dt.Rows[0]["name"].ToString();
+                rblbg.SelectedValue = dt.Rows[0]["bloodgroup"].ToString();
+                ddlqli.SelectedValue = dt.Rows[0]["qulification"].ToString();
+                ddlcountry.SelectedValue = dt.Rows[0]["country"].ToString();
+                showstate();
+                ddlstate.SelectedValue = dt.Rows[0]["state"].ToString();
+                btnsave.Text = "Update";
+                ViewState["kk"] = e.CommandArgument;
+            }
+        }
+
+        protected void ddlcountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(ddlcountry.SelectedValue=="0")
+            {
+                ddlstate.Enabled = false;
+            }
+            else
+            {
+
+            showstate();
+            ddlstate.Enabled = true;
+
+            }
+        }
+    }
+}
